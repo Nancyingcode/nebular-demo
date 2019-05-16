5d3633f2178f47fd431b663bfa51c682b6995c6c
Npro <yousayccc@gmail.com>
2019-05-16 18:55:12 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+import { NbThemeModule } from '@nebular/theme';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([]), // Router is required by Nebular
+    NbThemeModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
